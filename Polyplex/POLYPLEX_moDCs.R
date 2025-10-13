0981c2a0c6b76fe5cd5f2e78746fcc160e706ab7
Jeffrey Berry <jeffrey.berry@bayer.com>
2025-10-13 14:53:47 -0500

    god damn

--- Polyplex/POLYPLEX_moDCs.R
+++ Polyplex/POLYPLEX_moDCs.R
@@ -1,38 +1,22 @@
 library(ggh4x)
 library(ggplot2)
-
+library(scales)
+library(ggpubr)
 
 the.data <- read.csv("POLYPLEX_moDCs.csv", head = TRUE)
 the.data <- the.data[!duplicated(the.data),]
-head(the.data)
 the.data$Condition <- ordered(the.data$Condition, levels = c("10:1", "7.5:1", "5:1", "2.5:1", "1:1", "LPF3000", "free mRNA", "Blank"))
 the.data$Target.Name <- ordered(the.data$Target_Name, levels=c("Cells"))
-
-unique(the.data$Condition)
-unique(the.data$Target_Name)
-
-library(scales)
 the.data$Value <- as.numeric(the.data$Value)
-
-library(dplyr)
-mfi_summary <- the.data %>%
-  filter(Target_Name == "Cells") %>%
-  group_by(Condition) %>%
-  summarise(
-    mean_value = mean(Value, na.rm = TRUE),
-    se_value = sd(Value, na.rm = TRUE) / sqrt(n())
-
-is_theme_element()
-
-print(mfi_summary)
 
 # Reorder Group so "Polyplex" is on the left (first)
 the.data$Group <- factor(the.data$Group, levels = c("Polyplex", setdiff(unique(the.data$Group), "Polyplex")))
-
+my_comparisons <- list( c("free mRNA", "7.5:1"), c("LPF3000", "10:1"))
+    
 p <- ggplot(data=subset(the.data, Target_Name == "Cells"), aes(x=Condition, y=Value)) +
-facet_grid(~ Group, scales = "free_x", space = "free_x")+
-stat_summary(fun = mean, geom = "bar", position = position_dodge(), width = 0.7, fill = "white", color = "black") +
+  stat_summary(fun = mean, geom = "bar", position = position_dodge(), width = 0.7, fill = "white", color = "black") +
   stat_summary(fun.data = mean_se, geom = "errorbar", width = 0.2, position = position_dodge(.7)) +
+  stat_compare_means(comparisons = my_comparisons,method = "t.test")+
   geom_point(aes(color = Condition), position = position_jitter(width = 0.2), size = 1.5) +
   scale_fill_manual(values = c("Blank" = "#03d82d", "free mRNA" = "#8f1c07a6", "LPF3000" = "#ec6504", "10:1" = "#3e3a3ad6", "7.5:1" = "#851387", "5:1" = "#ec052c", "2.5:1" = "#070ee6", "1:1" = "#f30487")) +
   scale_color_manual(values = c("Blank" = "#03d82d", "free mRNA" = "#8f1c07a6", "LPF3000" = "#ec6504", "10:1" = "#3e3a3ad6", "7.5:1" = "#851387", "5:1" = "#ec052c", "2.5:1" = "#070ee6", "1:1" = "#f30487")) +
