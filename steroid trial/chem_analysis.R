e6ba798ef3270b89f2e3abb2134599dd9d6cd889
mrberry2 <matt.r.berry2@gmail.com>
2026-03-24 06:06:11 -0500

    updated data and plots

--- steroid trial/chem_analysis.R
+++ steroid trial/chem_analysis.R
@@ -1,48 +1,59 @@
 library(ggplot2)
 library(ggpubr)
 
-dat <- read.csv("Chem.data.file.csv", header = TRUE, stringsAsFactors = FALSE)
+dat <- read.csv("chem.data.file.csv", header = TRUE, stringsAsFactors = FALSE)
 
-not_nice_measures <- c("new_trait")
+not_nice_measures <- c("eACTH","BUN","ALP","cALP","ALT")
 
-for(measure in colnames(dat)[!colnames(dat) %in% c("Patient","Day")]){
-  df <- na.omit(dat[,c("Patient","Day",measure)])
+# Define patient groups
+patient_groups <- list(
+  list(patients = paste0("P", sprintf("%02d", 1:20)), days = c("Day 0", "Day 5")),
+  list(patients = paste0("P", sprintf("%02d", 21:40)), days = c("Day 0", "Day 14"))
+)
+
+for(group in patient_groups){
+  dat_filtered <- dat[dat$Patient %in% group$patients, ]
   
-  my_measure <- sym(measure)
-  
-  my_ylab <- sapply(my_measure,function(i){
-    if(i == "eACTH"){"eACTH (pg/mL)"}
-    else if(i == "BUN"){"BUN (mg/dL)"}
-    else if(i == "ALP"){"ALP (U/L)"}
-    else if(i == "cALP"){"cALP (U/L)"}
-    else if(i == "ALT"){"ALT (U/L)"}
-    else if(i == ""){""}
-    else{NA}
-  })
+  for(measure in colnames(dat_filtered)[!colnames(dat_filtered) %in% c("Patient","Day")]){
+    df <- na.omit(dat_filtered[,c("Patient","Day",measure)])
+    
+    my_measure <- sym(measure)
+    
+    my_ylab <- sapply(my_measure,function(i){
+      if(i == "eACTH"){"eACTH (pg/mL)"}
+      else if(i == "BUN"){"BUN (mg/dL)"}
+      else if(i == "ALP"){"ALP (U/L)"}
+      else if(i == "cALP"){"cALP (U/L)"}
+      else if(i == "ALT"){"ALT (U/L)"}
+      else{NA}
+    })
 
-  if(measure %in% not_nice_measures){
-    my_comparisons <- list(c("Day 0", "Day 5"))
-    my_breaks <- c("Day 0", "Day 5")
+    my_comparisons <- list(c(group$days[1], group$days[2]))
     my_width <- 4.12
-  }else{
-    my_comparisons <- list(c("Day 0", "Day 21"), c("Day 0", "Day 42"), c("Day 21", "Day 42"))
-    my_breaks <- c("Day 0","Day 21","Day 42")
-    my_width <- 6.67
-  }
 
-  p <- ggplot(df, aes(x=Day,y=!!my_measure))+
-    geom_line(aes(group=Patient),size=1.5,color="gray80")+
-    geom_point(aes(color=Patient),size=3)+
-    stat_compare_means(method = "t.test",comparisons = my_comparisons)+
-    theme_light()+
-    ylab(my_ylab)+
-    xlab("")+
-    scale_x_discrete(breaks=my_breaks)+
-    theme(axis.text = element_text(size = 18),
-      plot.title = element_text(size = 20),
-      axis.title= element_text(size = 18),
-      axis.text.x = element_text(angle = 45, hjust = 1))+
-    theme(legend.position = "right")
-  p
-  ggsave(paste0(my_measure,".png"),width = my_width,height = 4.82,dpi=300)
+    p <- ggplot(df, aes(x=Day,y=!!my_measure))
+    
+    # Add shaded region for eACTH detection limit
+    if(measure == "eACTH"){
+      p <- p + geom_rect(aes(xmin=-Inf, xmax=Inf, ymin=-Inf, ymax=5), 
+                         fill="#f8cae3", alpha=0.5, inherit.aes=FALSE)
+    }
+    
+    p <- p +
+      geom_line(aes(group=Patient),size=1.5,color="gray80")+
+      geom_point(aes(color=Patient),size=3)+
+      stat_compare_means(method = "t.test",comparisons = my_comparisons)+
+      theme_light()+
+      ylab(my_ylab)+
+      xlab("")+
+      scale_x_discrete(breaks=group$days)+
+      theme(axis.text = element_text(size = 18),
+        plot.title = element_text(size = 20),
+        axis.title= element_text(size = 18),
+        axis.text.x = element_text(angle = 45, hjust = 1))+
+      theme(legend.position = "right")
+    
+    ggsave(paste0(measure,"_",paste(group$patients[c(1,length(group$patients))],collapse="-"),".png"),
+           width = my_width,height = 4.82,dpi=300)
+  }
 }
